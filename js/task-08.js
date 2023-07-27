d2f1d78d6b97a6713e11a13ff826125f62d54512
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2023-07-27 12:35:25 +0200

    added 8+9 tasks

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,37 @@
+const form= document.querySelector('.login-form') 
 
+form.addEventListener('submit', handlerClickSubmit);
+
+function handlerClickSubmit(event) {
+    event.preventDefault();  // щоб не перезавантажувалося вікно при відправці форми
+    const { email, password } = event.currentTarget.elements;//    дестр-я
+   
+    // перевірка чи заповнені поля вводу
+    if (email.value === "" || password.value === '') {
+        alert('All fields must be filled')
+    }
+    else { console.log({"Email": email.value , "Password": password.value})
+      
+    form.reset()
+    }
+         
+    
+    
+}
+
+
+
+//  const formData = new FormData(event.currentTarget)
+//       // щоб перевірити що передає FormData
+//     formData.forEach((x, y) => {
+//         console.log('value',x);
+//             console.log('name', y);})
+    
+
+
+// console.log(formData);
+    // const formData = {
+    //         email,
+    //         password,
+    //     };console.log(formData);
+    
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const bodyEl = document.querySelector('body')
+const spanEl = document.querySelector('.color')
+const buttonEl = document.querySelector('.change-color')
+
+buttonEl.addEventListener('click', changeColor)
+
+function changeColor(event) {
+  bodyEl.style.backgroundColor = getRandomHexColor()    // прив`язати колір до body
+  spanEl.textContent = bodyEl.style.backgroundColor // прив`язати колір до span
+  
+}
+
+
