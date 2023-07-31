5315f04051ab34ff3d83bc72df9784c2698bed37
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2023-07-31 21:20:11 +0200

    fix5

--- js/task-05.js
+++ js/task-05.js
@@ -4,8 +4,10 @@ const nameOutput = document.querySelector('#name-output');
 nameInput.addEventListener('input',changeName);
 
 function changeName(event) {
-    nameOutput.textContent = "Anonymous";
+  nameOutput.textContent = "Anonymous";
+  console.log(event.currentTarget.value);
   if (event.currentTarget.value.trim() !== "") {
     nameOutput.textContent = event.currentTarget.value;
+    
   }
 }
