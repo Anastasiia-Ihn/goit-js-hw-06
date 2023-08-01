6a797505c206301a207f13eb7ec601d1e2fd1d7d
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2023-08-01 13:04:34 +0200

    fixed 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -12,27 +12,38 @@ listElementsControls[2].addEventListener('click', destroyBoxes)
 
 const boxesEl = document.querySelector('#boxes')
 
+let size = 20;
+
 function createBoxes(amount) {
 
-// передаємо значення введеного в інпут
-  amount = listElementsControls[0].value;
+  // передаємо значення введеного в інпут
+  amount = Number(listElementsControls[0].value);
   if (amount > 0 && amount <= 100) {
 
-//  Функція створює стільки <div>, скільки вказано в amount і додає їх у div#boxes.
+    //  Функція створює стільки <div>, скільки вказано в amount і додає їх у div#boxes.
+
+    const fragment = document.createDocumentFragment();
 
     for (let i = 0; i < amount; i += 1) {
       // ств квадрат
       const divElem = document.createElement('div');
-      divElem.style.width = 30 + (10 * i) + 'px';
-      divElem.style.height = 30 + (10 * i) + 'px';
+
+      size += 10
+      divElem.style.width = `${size}px`;
+      divElem.style.height = `${size}px`;
       divElem.style.backgroundColor = getRandomHexColor();
-      boxesEl.append(divElem)
+      fragment.append(divElem)      // всі div-и закинути в один блок
+
     }
+    listElementsControls[0].value = 0;
+    boxesEl.append(fragment)        // і цей блок вже закинути в код HTML
+
   }
 }
 
 function destroyBoxes() {
   boxesEl.innerHTML = '';
+  listElementsControls[0].value = 0;
 }
 
 
